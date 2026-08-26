13700073ba854e3ca092ff0f277ca0fe06b623f7
Juanma Gota <76853336+JohamnoGota@users.noreply.github.com>
2026-08-26 11:42:03 -0600

    Modified eda

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,19 +1,27 @@
 import pandas as pd
 import time 
+from functools import wraps
+import logging 
 
 def timeit(method):
     """
     Timeit Decorator 
-        Description: adds timekeeping functionality for any function, output is not modified and time is printed to terminal  
+        Description: adds timekeeping functionality for any logging function, usses the logger from the function 
     """
     # inner function that will be timed
+    @wraps(method)
     def timed(*args, **kwargs):
+        # get logger from function 
+        logger = logging.getLogger(method.__module__)
+
         ts = time.time()
         result = method(*args, **kwargs)
         te = time.time()
+
         # rounding to n decimals and printing timed output 
-        timed = round((te - ts), 5)
-        print(f"{method.__name__} took {timed} seconds to run")
+        elapsed = round((te - ts), 5)
+        logger.info(f"{method.__name__} took {elapsed} seconds to run")
+
         return result
     return timed
 
